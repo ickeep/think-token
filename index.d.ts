ab7fde57b92ea7c267c0e6ee3d0aae9be78d7d64
Keep <i@ickeep.com>
2019-02-02 00:48:47 +0800

    index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,44 @@
+declare module 'thinkjs' {
+  interface Context extends ThinkToken.TokenExtend {
+  }
+
+  interface Controller extends ThinkToken.TokenExtend {
+  }
+}
+type IName = string
+type IValue = string | { [key: string]: any }
+type IOpt = {
+  name?: string,
+  key?: string,
+  checkField?: string,
+  secret?: string,
+  cachePrefix?: string,
+  setCookie?: false
+}
+type IR = Promise<string | { [key: string]: any }>
+
+declare namespace ThinkToken {
+  interface TokenExtend {
+    /**
+     * get token
+     * @memberOf TokenExtend
+     */
+    token(name: IName): IR;
+
+    /**
+     * set token
+     * @memberOf TokenExtend
+     */
+    token(name: IName, value: IValue): IR;
+
+    /**
+     * delete the whole token
+     * @memberOf TokenExtend
+     */
+    token(name: null): IR;
+
+    clearToken(name: IName, id: string, opts?: IOpt): void
+  }
+}
+
+export = ThinkToken;
